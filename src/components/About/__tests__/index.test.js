c2e120dbd63eac360d21dfc419e07f1acb1f8453
techman <dmoloney5@gmail.com>
2022-05-11 14:06:50 -0400

    About test working

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { render, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About Component', () => {
+    //first test
+    it('render', () => {
+        render(<About />);
+    });
+
+    //second test
+    it('matches snapshot DOM node structure', () => {
+        //render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
