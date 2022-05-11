cb31dfe578869b8c98249fb6fe9d906642866565
techman <dmoloney5@gmail.com>
2022-05-11 14:34:59 -0400

    test feature completed

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,23 +1,19 @@
-import React from "react";
-
+// __tests__/About.test.js
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
 
 afterEach(cleanup);
 
-describe('About Component', () => {
-    //first test
-    it('render', () => {
-        render(<About />);
-    });
-
-    //second test
-    it('matches snapshot DOM node structure', () => {
-        //render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
